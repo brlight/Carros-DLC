891f5b4e4caa04ee7f967574418683c3bd699fcd
Dom GiGi <38845277+DomGiGi@users.noreply.github.com>
2018-09-18 19:11:19 +0100

    Add files via upload

---
+++ Novos-Carros-DLC-master/Novas Viaturas/Novas Viaturas Dlc.sql
@@ -0,0 +1,19 @@
+INSERT INTO `vehicles` (name, model, price, category) VALUES
+  ('Stafford', 'STAFFORD', 65500, 'sedans'),
+  ('SPEEDO 4', 'SPEEDO4', 62500, 'vans'),
+  ('PATRIOT 2', 'PATRIOT2', 62500, 'suvs'),
+  ('Ocelot Swinger', 'swinger', 62500, 'sportsclassics'),
+  ('Dinka Jester', 'jester3', 62500, 'sportsclassics'),
+  ('Michelli GT', 'MICHELLI', 62500, 'sportsclassics'),
+  ('Tyrant', 'Tyrant', 62500, 'super'),
+  ('DOMINATOR GTX', 'DOMINATOR3', 62500, 'muscle'),
+  ('Issi Classic', 'ISSI3', 62500, 'compacts'),
+  ('Tezeract', 'TEZERACT', 62500, 'super'),
+  ('Ellie', 'ellie', 62500, 'muscle'),
+  ('GB200', 'GB200', 62500, 'sports'),
+  ('Taipan', 'Taipan', 62500, 'super'),
+  ('Overflod Entity', 'entity2', 62500, 'super'),
+  ('Hotring Sabre', 'HOTRING', 62500, 'sports'),
+  ('Fagaloa', 'Fagaloa', 62500, 'sportsclassics'),
+  ('Flash GT', 'FlashGT', 62500, 'compacts')
+;
---
+++ Novos-Carros-DLC-master/README.md
@@ -0,0 +1,3 @@
+# Novos-Carros-DLC
+
+# Basta Dropar Ficheiro Sql Na DataBase e as  Viaturas estao Disponiveis.
